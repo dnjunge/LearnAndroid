b880a538bad5ec912b7f3a229c5207dc186dae3e
NJUNDA10 <NJUNDA10@MD15ZRUC.ww005.siemens.net>
2014-11-23 02:14:13 -0500

    commit

--- DanApp/src/com/dansonn/danapp/MainActivity.java
+++ DanApp/src/com/dansonn/danapp/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+       MenuInflater inflater = getMenuInflater();
+       inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -31,11 +32,20 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+       /* int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
-        }
-        return super.onOptionsItemSelected(item);
+        }*/
+    	switch (item.getItemId()){
+    	case R.id.action_search:
+    		openSearch();
+    		return true;
+    	case R.id.action_settings:
+    		openSettings();
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
     }
     
     //Called when the user clicks the send button
